32079c0cc3d7eacc42d419d15224cf66f4629af9
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-02 16:41:12 +0300

    Updated Code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux'
 
 import './index.css';
 import App from './App';
+import Store from ''
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
