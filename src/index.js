a6b025bfe5e97fcbc962fbb27c487d1421227b2c
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-05 11:36:32 +0300

    Updated Code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
 
+import store from './store/index';
 import './index.css';
 import App from './App';
-import Store from '.store/index'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Provider store={Store}><App /></Provider>);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
 
-import uiSlice from './ui-slice'
-import cartSlice from './cart-slice'
+import uiSlice from '';
+import cartSlice from './cart-slice';
 
 const store = configureStore({
-    reducer: {
-        ui: uiSlice.reducer,
-        cart: cartSlice.reducer
-    }
-})
+  reducer: { ui: uiSlice.reducer, cart: cartSlice.reducer },
+});
 
-export default store
+export default store;
