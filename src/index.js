4b57c6c4527167e4fe66c94c8e3b96f3c014431b
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-05 08:56:57 +0300

    Updated Code

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux'
 
 import './index.css';
 import App from './App';
-import store from '.store/index'
+import Store from '.store/index'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Provider store={store}><App /></Provider>);
+root.render(<Provider store={Store}><App /></Provider>);
