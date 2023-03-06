499207a29f4bb3893556f5d26394f0e877b88935
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-06 11:05:18 +0300

    Updated Code

--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -1,15 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const uiSlice = createSlice({
-    name: 'ui',
-    initialState: { cartIsVisible: false },
-    reducers: {
-        toggle(state) {
-            state.cartIsVisible = !state.cartIsVisible
-        }
-    }
-})
+  name: 'ui',
+  initialState: { cartIsVisible: false, notification: null },
+  reducers: {
+    toggle(state) {
+      state.cartIsVisible = !state.cartIsVisible;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
+  },
+});
 
-export const uiActions = uiSlice.actions
+export const uiActions = uiSlice.actions;
 
-export default uiSlice
+export default uiSlice;
