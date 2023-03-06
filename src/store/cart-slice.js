0b494b901ddda72f7d95e93f0772b58e1603d74f
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-06 11:39:01 +0300

    Updated code

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,7 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-import { uiActions } from './ui-slice';
-
 const cartSlice = createSlice({
   name: 'cart',
   initialState: {
@@ -42,52 +40,6 @@ const cartSlice = createSlice({
     },
   },
 });
-
-export const sendCartData = (cart) => {
-  return async (dispatch) => {
-    dispatch(
-      uiActions.showNotification({
-        status: 'pending',
-        title: 'Sending...',
-        message: 'Sending cart data!',
-      })
-    );
-
-    const sendRequest = async () => {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/cart.json',
-        {
-          method: 'PUT',
-          body: JSON.stringify(cart),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Sending cart data failed.');
-      }
-    };
-
-    try {
-      await sendRequest();
-
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'Success!',
-          message: 'Sent cart data successfully!',
-        })
-      );
-    } catch (error) {
-      dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Sending cart data failed!',
-        })
-      );
-    }
-  };
-};
 
 export const cartActions = cartSlice.actions;
 
--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -2,19 +2,12 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const uiSlice = createSlice({
   name: 'ui',
-  initialState: { cartIsVisible: false, notification: null },
+  initialState: { cartIsVisible: false },
   reducers: {
     toggle(state) {
       state.cartIsVisible = !state.cartIsVisible;
-    },
-    showNotification(state, action) {
-      state.notification = {
-        status: action.payload.status,
-        title: action.payload.title,
-        message: action.payload.message,
-      };
-    },
-  },
+    }
+  }
 });
 
 export const uiActions = uiSlice.actions;
