90257701db4637af9ae85055743babc3e739892a
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-06 10:41:45 +0300

    Updated Code

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
@@ -5,6 +6,11 @@ import Products from './components/Shop/Products';
 
 function App() {
   const showCart = useSelector( state => state.ui.cartIsVisible)
+  const cart = useSelector(state => state.cart)
+
+  useEffect(() => {
+    fetch('https://react-http-6b4a6.firebaseio.com/cart.json', {method: 'PUT', body:JSON.stringify(cart),})
+  }, [cart])
   return (
     <Layout>
       {showCart && <Cart />}
--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,55 +1,26 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { cartActions } from '../../store/cart-slice';
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
 
 const ProductItem = (props) => {
-  const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
 
   const { title, price, description, id } = props;
 
   const addToCartHandler = () => {
-    const newTotalQuantity = cart.totalQuantity + 1;
-
-    const updatedItems = cart.items.slice(); // create copy via slice to avoid mutating original state
-    const existingItem = updatedItems.find((item) => item.id === id);
-    if (existingItem) {
-      const updatedItem = { ...existingItem }; // new object + copy existing properties to avoid state mutation
-      updatedItem.quantity++;
-      updatedItem.totalPrice = updatedItem.totalPrice + price;
-      const existingItemIndex = updatedItems.findIndex(
-        (item) => item.id === id
-      );
-      updatedItems[existingItemIndex] = updatedItem;
-    } else {
-      updatedItems.push({
-        id: id,
-        price: price,
-        quantity: 1,
-        totalPrice: price,
-        name: title,
-      });
-    }
-
-    const newCart = {
-      totalQuantity: newTotalQuantity,
-      items: updatedItems,
-    };
-
-    dispatch(cartActions.replaceCart(newCart));
-
+   
     // and then send Http request
     // fetch('firebase-url', { method: 'POST', body: JSON.stringify(newCart) })
 
-    // dispatch(
-    //   cartActions.addItemToCart({
-    //     id,
-    //     title,
-    //     price,
-    //   })
-    // );
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
   };
 
   return (
